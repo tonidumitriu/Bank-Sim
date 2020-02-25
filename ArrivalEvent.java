478e27c656dd795120296e92d6768a6dc3fb5c8c
tonidumitriu <59662322+tonidumitriu@users.noreply.github.com>
2020-02-25 18:33:15 -0500

    Update ArrivalEvent.java

--- ArrivalEvent.java
+++ ArrivalEvent.java
@@ -1,9 +1,3 @@
-// CS 0445 Spring 2020
-// Simply a subclass with a constructor, and toString(), but no other
-// methods.
-
-// This subclass is simply to distinguish it from other event types.
-
 public class ArrivalEvent extends SimEvent
 {
     public ArrivalEvent(double new_time)
