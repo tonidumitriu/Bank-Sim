8f72c9794da0d5eb0583cf5b7365d1ff8600a596
tonidumitriu <59662322+tonidumitriu@users.noreply.github.com>
2020-02-25 18:37:03 -0500

    Update SimEvent.java

--- SimEvent.java
+++ SimEvent.java
@@ -1,18 +1,3 @@
-// CS 0445 Spring 2020
-// This class sets up the structure and functionality of events.
-
-// The primary attribute of an event is its time stamp.  This will
-// be used to compare / order events in a system.
-
-// By extending this class for different types of events, we enable
-// the different types of events to be compatible but still
-// distinguishable in an object-oriented way.  Since the time values are
-// double, we do not directly compare for equality -- rather we test to see
-// if the difference between the two is very small.
-
-// Note that this class is abstract -- we will not create any SimEvent
-// objects in our program. Rather, we will create objects of its subclasses.
-
 public abstract class SimEvent implements Comparable<SimEvent>
 {
     protected double e_time;  // Time when event was generated
